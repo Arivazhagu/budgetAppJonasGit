650bbdc7aa5a2c8967c16360c668fe9adc195074
Arivazhagu <54696046+Arivazhagu@users.noreply.github.com>
2020-06-10 08:44:30 +0530

    ES Six starter

---
+++ budgetGit/budgetGit/esSixJs.js
@@ -0,0 +1,8 @@
+﻿console.log("Application has started!");
+// let and constants
+
+//Const values cannot be reassigned
+
+// let and const are block scoped
+
+//same name can be used inside and outside loop, inside the loop it is initiated with let key word. Both variables will become different
